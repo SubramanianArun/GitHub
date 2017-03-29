bdab8931b3547152b500cb65eccad69744a5080d
Subramanian Arunachalam <subramanian.a@outlook.com>
2017-03-29 12:25:14 -0500

    Updates 03/29/17

---
+++ team.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>PowerCyber Labs - Iowa State University</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+    <!-- Plugin CSS -->
+    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+    <!-- Theme CSS -->
+    <link href="css/creative.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body id="page-top">
+
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand page-scroll" href="/powercyber/welcome.php#page-top">Power Cyber Labs</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a class="page-scroll" href="/powercyber/team.php#page-top">The Team</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="/powercyber/welcome.php#tutorials">Experiments & Training</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="/powercyber/welcome.php#contact">Downloads</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="/powercyber/welcome.php#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+  <header class="customheader">
+  </header>
+
+      <section id="intro">
+          <div class="container">
+              <div class="row">
+                  <div class="col-lg-12 text-center">
+                      <h2 class="section-heading">ISU PowerCyber Lab</h2>                    
+                      <p>PowerCyber lab of Iowa State University has been under construction with collaborated efforts for ten years. Our research mainly focuses on Cyber-physical systems framework for risk modeling, risk mitigation, and attack-resilient control algorithms that account for dynamics of the physical system as well as the operational aspects of the cyber-based control network. Besides, the lab is also remotely available to run various experiments for different types of users such as universities, companies and national institutes</p>
+                  </div>
+              </div>
+          </div>
+      </section>
+      <section id="team">
+          <div class="container">
+            <div class="panel panel-default">
+                <div class="panel-heading">Faculty</div>
+                <div class="panel-body">
+                  <ul>
+                    <li><a class="nav-link" href="http://ecpe.ece.iastate.edu/gmani">Dr. Manimaran Govindarasu (Principal Investigator)</a></li><br>
+                    <li><a class="nav-link" href="http://www.engineering.iastate.edu/directory/?user_page=vajjarap">Dr. Venkataramana Ajjarapu</a></li><br>
+                    <li><a class="nav-link" href="http://www.dougj.net/">Dr. Doug Jacobson</a></li><br>
+                    <li><a class="nav-link" href="http://home.eng.iastate.edu/~ugvaidya/team.html">Dr. Umesh Vaidya</a></li><br>
+                    <li><a class="nav-link" href="http://school.eecs.wsu.edu/node/1046">Dr. Chen-Ching Liu (Collaborator, Washington State University)</a></li><br>
+                </ul>
+                </div>
+            </div>
+          </div>
+          <div class="container">
+            <div class="panel panel-default">
+                <div class="panel-heading">Current Students</div>
+                <div class="panel-body">
+                  <ul>
+                    <li>Ashok, Aditya, Ph.D.</li><br>
+                    <li>Wang, Pengyuan, Ph.D.</li><br>
+                    <li>Singh, Vivek Kumar, Ph.D.</li><br>
+                    <li>Arunachalam, Subramanian, M.S.</li><br>
+                    <li>Sarangan, Sri Krishna, M.S.</li><br>
+                    <li>Perez Callupe, Steven V, M.S.</li><br>
+                    <li>Drahos, Jacob M, M.S.</li><br>
+                    <li>Ulrich, Jacob J, M.S.</li><br>
+                    <li>Ebrahem, Haythem E, M.S.</li><br>
+                </ul>
+                </div>
+            </div>
+          </div>
+          <div class="container">
+            <div class="panel panel-default">
+                <div class="panel-heading">Previous Students</div>
+                <div class="panel-body">
+                  <ul>
+                    <li>Adam Hahn, Ph.D.</li><br>
+                    <li>Siddharth Sridhar, Ph.D.</li><br>
+                    <li>Chee-Wooi Ten, Ph.D.</li><br>
+                    <li>Mohammad Fraiwan, Ph.D.</li><br>
+                    <li>Jie Yan, Ph.D.</li><br>
+                    <li>Srdjan Pudar, M.S.</li><br>
+                    <li>Brown, Matthew, M.S.</li><br>
+                    <li>Krishnaswamy, Sujatha, M.S.</li><br>
+                    <li>Pappa, Aswin Chidambaram, M.S.</li><br>
+                    <li>Ryan Eikanger, REU Student</li><br>
+                </ul>
+                </div>
+            </div>
+          </div>
+      </section>
+
+
+    <!-- jQuery -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+    <script src="vendor/scrollreveal/scrollreveal.min.js"></script>
+    <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="js/creative.min.js"></script>
+
+</body>
+</html>
